9116ccc7131905a09579c5a4a259b64dd2fca092
Graham Lavender <glavender@gmail.com>
2014-01-23 18:13:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BikeableToronto::Application.config.secret_key_base = '876218c9528f6c533414a4b9518548d00349b5d4fe3c25aa1585ee7bd8ca3fd9e8e944d5c34a40935c2a90eb342c76984898228f6375b361068c3e3306c1c6eb'
