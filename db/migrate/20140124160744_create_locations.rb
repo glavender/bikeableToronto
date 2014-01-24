7e213f8d43107efa64e6632a13efe53441ebda6a
Graham Lavender <glavender@gmail.com>
2014-01-24 11:09:44 -0500

    Creating location model

---
+++ db/migrate/20140124160744_create_locations.rb
@@ -0,0 +1,12 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.string :name
+      t.text :description
+      t.text :address
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
