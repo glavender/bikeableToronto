b76ad5030aa949a1801332dfe88341600d95185b
Graham Lavender <glavender@gmail.com>
2014-01-26 22:35:41 -0500

    Adding seeds file with two locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 BikeableToronto::Application.routes.draw do
+  root :to => 'locations#index'
+
+  resources :locations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Location.create(name: 'Cyclepath', description: 'Specializes in Norco, but sells bikes, parts, and accessories for various brands.', address: '2106 Yonge St, Toronto, ON M4S 2A5', image: 'http://cyclepathtoronto.com/merchant/2139/images/site/CyclepathLogoblackabove.jpg')
+Location.create(name: 'Mountain Equipment Coop', description: 'Covers all sorts of outdoor activities, including biking', address: '400 King Street West, Toronto, ON', image: 'http://images.mec.ca/media/img/tpl/glb/mec_logo_new.gif')
